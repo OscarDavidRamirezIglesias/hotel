d7d5ffa1bc9d25b834302ca4410452df6e83cb54
OscarDavidRamirezIglesias <130012138+OscarDavidRamirezIglesias@users.noreply.github.com>
2024-01-18 16:08:57 -0500

    Se agregaron las 3 entidades necesarias.

--- src/main/java/com/hotel/model/dao/HuespedDao.java
+++
@@ -1,7 +0,0 @@
-package com.hotel.model.dao;
-
-import com.hotel.model.entity.Huesped;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HuespedDao extends CrudRepository<Huesped, Integer> {
-}
